4619110f0fb4ac4fb95308178da589364de1b3f7
李健 <jian.li04@ele.me>
2017-05-24 14:16:18 +0800

    remove swagger

--- main.go
+++ main.go
@@ -13,10 +13,10 @@ func main() {
 		AllowMethods:     []string{"POST", "GET", "UPDATE", "DELETE"},
 		AllowCredentials: true,
 	}))
-	// if beego.BConfig.RunMode == "dev" {
-	beego.BConfig.WebConfig.DirectoryIndex = true
-	beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
-	beego.BConfig.WebConfig.StaticDir["/static"] = "static"
-	// }
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+		beego.BConfig.WebConfig.StaticDir["/static"] = "static"
+	}
 	beego.Run()
 }
