80efdc2143a6b8837f450c4e31d55d016c587090
李健 <jian.li04@ele.me>
2017-05-31 09:15:33 +0800

    add headurl

--- models/user.go
+++ models/user.go
@@ -32,8 +32,8 @@ type Player struct {
 	Role    int    `db:"role"`
 	Score   int    `db:"score"`
 	NicName string `db:"nickname"`
-	Head    string
-	Active  bool `db:"active"`
+	Head    string `db:"head"`
+	Active  bool   `db:"active"`
 }
 
 type UserReq struct {
