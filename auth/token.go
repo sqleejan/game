f5f99c6e1de8f9a222b0ddfb863fa3b9a1bc064e
李健 <jian.li04@ele.me>
2017-06-02 18:46:33 +0800

    token log

--- auth/token.go
+++ auth/token.go
@@ -57,7 +57,6 @@ type MyCustomClaims struct {
 
 func (mc *MyCustomClaims) Token() string {
 	claims := mc.StandardClaims
-	refreshToken.Add(claims.Id)
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	ob, _ := token.SignedString(rootkey)
 	return ob
@@ -183,5 +182,6 @@ func WXClaim(code string) (*MyCustomClaims, error) {
 	claims.Audience = user.Nickname
 	claims.Issuer = user.HeadImageURL
 	//claims.Subject = rid
+	refreshToken.Add(claims.Id)
 	return claims, nil
 }
